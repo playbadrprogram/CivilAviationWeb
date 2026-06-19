65114480381857ee11b7e8c0b55720a757a79c49
Eng.Badr <playbadr11@gmail.com>
2026-06-19 18:25:44 +0300

    Refactor game.js for improved readability

--- game.js
+++ game.js
@@ -1,4 +1,4 @@
-import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
+٨import * as THREE from 'https://unpkg.com/three@0.160.0/build/three.module.js';
 
 const scene = new THREE.Scene();
 
